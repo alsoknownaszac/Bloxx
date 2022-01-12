ed1858e0a0722cc593c4e05514acaac91f8ac1ec
Amayo <mayo16collins@gmail.com>
2022-01-12 17:28:03 +0100

    90% completion  @zac

--- components/index.js
+++ components/index.js
@@ -8,3 +8,4 @@ export { default as PostCard } from "./PostCard";
 export { default as PostDetail } from "./PostDetail";
 export { default as PostWidget } from "./PostWidget";
 export { default as FeaturedPostCard } from "./FeaturedPostCard";
+export { default as Loader } from "./Loader";
