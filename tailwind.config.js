0bb86bf71a7331eb90191336e25466b3ee7ce07d
Amayo <mayo16collins@gmail.com>
2022-04-14 12:26:07 +0100

    general edits

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
+    screens: {
+      xs: "475px",
+      ...defaultTheme.screens,
+    },
     extend: {
       fontFamily: {
         welcome: ['"Poiret One"', "Cursive"],
